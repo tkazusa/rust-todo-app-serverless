d4d84d0b5a820a5e324ed5b76accade8960b28a7
Ubuntu <ubuntu@ip-172-31-30-86.ap-northeast-1.compute.internal>
2021-02-11 04:45:07 +0000

    feat: mod basic authorization responce

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use askama::Template;
 use lamedh_runtime::run;
-use lamedh_http::{
-    lambda::{Context, Error},
-    IntoResponse, Request, Response, handler
-};
+use lamedh_http::{IntoResponse, Request, Response, handler, http::header::HeaderValue, lambda::{Context, Error}};
  
 use rusoto_core::Region;
 use rusoto_dynamodb::DynamoDbClient;
 
 mod dynamodb_operations;
 use crate::dynamodb_operations::scan;
+use std::collections::HashMap;
+
+use base64::{encode, decode};
 
 // ToDo のリストを持つ構造体
 struct TodoEntry {
@@ -32,26 +32,64 @@ async fn main() -> Result<(), Error> {
 }
 
 // lamedh_http::handler の要求する型は、 Result<Self::Response, Self::Error>>
-async fn func(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    let mut entries = Vec::new();
+async fn func(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
 
-    let client = DynamoDbClient::new(Region::ApNortheast1);
-    let items_vector = scan(client).await.items.unwrap();
+    if check_authorization_header(event) {
+        let mut entries = Vec::new();
+        let client = DynamoDbClient::new(Region::ApNortheast1);
+        let items_vector = scan(client).await.items.unwrap();
 
-    for item in items_vector.iter(){
-        entries.push(TodoEntry{
-            id: item["id"].s.as_ref().unwrap().to_string(),
-            text: item["text"].s.as_ref().unwrap().to_string()
-        })};
+        for item in items_vector.iter(){
+            entries.push(TodoEntry{
+                id: item["id"].s.as_ref().unwrap().to_string(),
+                text: item["text"].s.as_ref().unwrap().to_string()
+            })};
 
-    // IndexTemplate は Template から derive してる、多分 html.render で HTML を生成
-    let html = IndexTemplate { entries };
-    let response_body = html.render()?;
+        // IndexTemplate は Template から derive してる、多分 html.render で HTML を生成
+        let html = IndexTemplate { entries };
+        let response_body = html.render()?;
 
-    // Ok の中で実施されたものが、成功した場合、返り値として Value を返す。
-    Ok(Response::builder()
-        .status(401)
-        .header("WWW-Authenticate", "Basic")
-        .body("Unauhorized!!!")
-        .expect("failed to render response"))
+        Ok(Response::builder()
+            .status(200)
+            .header("Content-Type", "text/html; charset=UTF-8")
+            .body(response_body)
+            .expect("failed to render response"))
+    } else {
+
+        Ok(Response::builder()
+            .status(401)
+            .header("WWW-Authenticate", "Basic")
+            .body("Unauthorized".to_string())
+            .expect("failed to render response")) 
+    }
+}
+
+fn check_authorization_header(event: Request) -> bool {
+    let mut mapd = HashMap::new();
+    mapd.insert("user1", "pass1");
+    
+    let header_auth = event.headers().get("authorization");
+    
+    match header_auth {
+        // header の中身が入っていた場合を評価
+        Some(header_auth)  => {
+            let auth_value: &str = header_auth.to_str().unwrap();
+            // 登録されている user と pass のタプルで loop 
+            for (user, pass) in mapd.iter(){
+                let str = format!("{}:{}", user, pass);
+                let encoded_value = encode(str);
+                let check_value = format!("Basic {}", encoded_value);
+                // 入力と登録されていたものが一致すれば true
+                if &auth_value ==  &check_value {
+                    return true;
+                } else {
+                    continue;
+                }
+            }
+            // 一致したものがなければ false
+            return false;
+        }
+        // そもそも autorization の中身が空なら false
+        None => return false,
+    }
 }
